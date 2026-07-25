951be23c52f0f70e2dcede81caa3283089d97af6
YadaK06 <tonfaiyada@gmail.com>
2026-07-25 16:48:44 +0700

    full

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -20,11 +20,18 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private BallColor color;
 
+    private MeshRenderer rb;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         GameManager.instance.PlayScore += point;
         Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<MeshRenderer>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -37,5 +44,51 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     void Update()
     {
         
+    }
+
+    public void SetColor(BallColor col)
+    {
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rb.material.color = Color.white;
+                break;
+
+            case BallColor.Red:
+                point = 1;
+                rb.material.color = Color.red;
+                break;
+
+            case BallColor.Yellow:
+                point = 2;
+                rb.material.color = Color.yellow;
+                break;
+
+            case BallColor.Green:
+                point = 3;
+                rb.material.color = Color.green;
+                break;
+
+            case BallColor.Brown:
+                point = 4;
+                rb.material.color = Color.brown;
+                break;
+
+            case BallColor.Blue:
+                point = 5;
+                rb.material.color = Color.blue;
+                break;
+
+            case BallColor.Pink:
+                point = 6;
+                rb.material.color = Color.pink;
+                break;
+
+            case BallColor.Black:
+                point = 7;
+                rb.material.color = Color.black;
+                break;
+        }
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,6 +7,12 @@ public class GameManager : MonoBehaviour
     private int playerScore;
     public int PlayScore {  get { return playerScore; } set { playerScore = value; } }
 
+    [SerializeField]
+    private GameObject[] ballPositons;
+    
+    [SerializeField]
+    private GameObject ballPrefabs;
+
     public static GameManager instance;
 
     void Awake()
@@ -17,12 +23,27 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefabs,
+                                     ballPositons[i].transform.position,
+                                     Quaternion.identity);
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColor(col);
     }
 }
